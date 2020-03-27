40af7d4c8db7bf8c1936069a6398680cf75e7a02
srilakshmi85 <33021941+srilakshmi85@users.noreply.github.com>
2020-03-27 19:04:22 +0530

    Add files via upload

--- Member.php
+++ Member.php
@@ -9,7 +9,7 @@ class Member
 
     function __construct()
     {
-        require_once __DIR__ . '/DataSource.php';
+        require_once  './DataSource.php';
         $this->ds = new DataSource();
     }
 
