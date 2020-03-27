a8390a78b2e334f2d0900637a2a04e099d074a09
srilakshmi85 <33021941+srilakshmi85@users.noreply.github.com>
2020-03-27 18:37:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+use Phppot\Member;
+
+session_start();
+?>
+<HTML>
+<HEAD>
+<TITLE>user-registration</TITLE>
+<link href="/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="/user-registration.css" type="text/css"
+	rel="stylesheet" />
+<script src="/jquery-3.3.1.js" type="text/javascript"></script>
+</HEAD>
+<BODY>
+	<div class="phppot-container">
+	<?php require_once "login-form.php";?>
+	</div>
+</BODY>
+</HTML>
