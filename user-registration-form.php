741f84ffad0dd23af5904b17069a8b7cefe8a7a8
srilakshmi85 <33021941+srilakshmi85@users.noreply.github.com>
2020-03-27 18:53:56 +0530

    Add files via upload

--- user-registration-form.php
+++ user-registration-form.php
@@ -1,6 +1,8 @@
 <?php
 echo "here:";
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 if (! empty($_POST["signup-btn"])) {
     require_once './Member.php';
