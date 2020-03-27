33ee891fbfbf5d6b11ffe62d81f0c76a7f2e8ddc
srilakshmi85 <33021941+srilakshmi85@users.noreply.github.com>
2020-03-27 18:39:12 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ session_start();
 <HTML>
 <HEAD>
 <TITLE>user-registration</TITLE>
-<link href="/phppot-style.css" type="text/css"
+<link href="./phppot-style.css" type="text/css"
 	rel="stylesheet" />
-<link href="/user-registration.css" type="text/css"
+<link href="./user-registration.css" type="text/css"
 	rel="stylesheet" />
-<script src="/jquery-3.3.1.js" type="text/javascript"></script>
+<script src="./jquery-3.3.1.js" type="text/javascript"></script>
 </HEAD>
 <BODY>
 	<div class="phppot-container">
